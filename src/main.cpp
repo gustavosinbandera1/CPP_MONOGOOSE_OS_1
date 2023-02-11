43f9935d1b1b0f0b77c8ed5685c62a345dcb2ba9
gustavosinbandera1 <gustavosinbandera1@hotmail.com>
2023-02-11 12:55:49 -0500

    removed unnnecessary code

--- src/main.cpp
+++ src/main.cpp
@@ -50,68 +50,6 @@ static void connected_cb(int ev, void *evd, void *arg) {
 }
 
 
-static void my_timer_cb2(void *arg) {
-  // bool val = mgos_gpio_toggle(2);
-
-  int status = GPIO_CONTROLLER->read_gpio(32);
-   LOG(LL_INFO, ("pin 32 status: %d", status));
-
-  static int x = 0;
-
-  if (x % 2) {
-    GPIO_CONTROLLER->write_gpio(2, true);
-
-  } else {
-    GPIO_CONTROLLER->write_gpio(2, false);
-  }
-  x++;
-
-  char topic[100];
-  snprintf(topic, sizeof(topic), "/devices/%s/events",
-           mgos_sys_config_get_device_id());
-
-  bool res = mgos_mqtt_pubf(topic, 0, false /* retain */,
-                            "{total_ram: %lu, free_ram: %lu}",
-                            (unsigned long) mgos_get_heap_size(),
-                            (unsigned long) mgos_get_free_heap_size());
-
-  char buf[8];
-  LOG(LL_INFO, ("published: %s", res ? "yes" : "no"));
-
-  LOG(LL_INFO, ("topic: %s", topic));
-  std::cout << "\033[1;31mbold red text\033[0m\n";
-  (void) arg;
-}
-
-static void my_timer_cb3(void *arg) {
-  char topic[100];
-  static int var = 32;
-  int var2 = 1;
-  float testFloat = 3.3456789;
-
-  char message[100];
-  snprintf(message, sizeof(message), "sensor: %d", 4);
-
-
-  snprintf(topic, sizeof(topic), "/devices/%s/test",
-           mgos_sys_config_get_device_id());
-  bool res = mgos_mqtt_pubf(topic, 0, false /* retain */,
-                            "{count_ascii: %c, count2: %d, test_float : %f}",
-                            (int) var++, var2++, testFloat);
-
-  char buf[8];
-  LOG(LL_INFO, ("published: %s", res ? "yes" : "no"));
-
-  LOG(LL_INFO, ("topic: %s", topic));
-  std::cout << "\033[1;31mbold red text\033[0m\n";
-  (void) arg;
-}
-
-static void mifunc(struct mg_connection *c, const char *topic, int topic_len,
-                   const char *msg, int msg_len, void *userdata) {
-  LOG(LL_INFO, ("Got message on topic %.*s", msg_len, msg));
-}
-
 extern "C" enum mgos_app_init_result mgos_app_init(void) {
 
   mgos_event_add_handler(BUTTON_PRESSED, btn_cb, NULL );
@@ -120,12 +58,7 @@ extern "C" enum mgos_app_init_result mgos_app_init(void) {
   mgos_event_add_group_handler(MGOS_EVENT_GRP_NET, my_net_ev_handler, NULL);
   //mgos_event_add_handler(MGOS_NET_EV_CONNECTED, connected_cb, NULL );
 
-  mgos_set_timer(3000, MGOS_TIMER_REPEAT, my_timer_cb2, NULL);
-  mgos_set_timer(3000, MGOS_TIMER_REPEAT, my_timer_cb3, NULL);
-
-  mgos_mqtt_sub("/devices/data", mifunc, NULL);
   GPIO_CONTROLLER->config_gpio(2, MGOS_GPIO_MODE_OUTPUT);
-
   GPIO_CONTROLLER->config_gpio(mgos_sys_config_get_buttons_button1(), MGOS_GPIO_MODE_INPUT);
   GPIO_CONTROLLER->set_gpio_isr(mgos_sys_config_get_buttons_button1(), MGOS_GPIO_PULL_UP, MGOS_GPIO_INT_EDGE_NEG, NULL);
 
